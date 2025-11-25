ad0fe187efc87853f1ad5057bbbd024259e359d0
Richard Liu <richardadesigner@gmail.com>
2025-11-25 11:19:58 -0800

    update

--- script.js
+++ script.js
@@ -3,11 +3,36 @@ function enterText(text) {
     text = text.toString();
     topBox.value = topBox.value + text
 }
-for (let i = 0; i <= 9; i++) {
-    document.getElementById(i).addEventListener("click", function () {
-        enterText(i)
-    })
-}
+document.getElementById("zero").addEventListener("click", function () {
+    enterText("0")
+});
+document.getElementById("1").addEventListener("click", function () {
+    enterText("1")
+});
+document.getElementById("2").addEventListener("click", function () {
+    enterText("2")
+});
+document.getElementById("3").addEventListener("click", function () {
+    enterText("3")
+});
+document.getElementById("4").addEventListener("click", function () {
+    enterText("4")
+});
+document.getElementById("5").addEventListener("click", function () {
+    enterText("5")
+});
+document.getElementById("6").addEventListener("click", function () {
+    enterText("6")
+});
+document.getElementById("7").addEventListener("click", function () {
+    enterText("7")
+});
+document.getElementById("8").addEventListener("click", function () {
+    enterText("8")
+});
+document.getElementById("9").addEventListener("click", function () {
+    enterText("9")
+});
 document.getElementById("+").addEventListener("click", function () {
     enterText("+")
 });
